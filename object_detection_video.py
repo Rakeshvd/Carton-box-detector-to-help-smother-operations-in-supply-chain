9da1bdb825cdc7e92a99bd20ab4e70d8727fe890
Rakesh Dhotre <rakeshvdhotre@gmail.com>
2019-10-27 17:08:15 +0530

    Update object_detection_video.py

--- object_detection_video.py
+++ object_detection_video.py
@@ -1,60 +1,37 @@
 ######## Video Object Detection Using Tensorflow-trained Classifier #########
-#
-# Author: Evan Juras
-# Date: 1/16/18
-# Description: 
-# This program uses a TensorFlow-trained classifier to perform object detection.
-# It loads the classifier uses it to perform object detection on a video.
-# It draws boxes and scores around the objects of interest in each frame
-# of the video.
-
-## Some of the code is copied from Google's example at
-## https://github.com/tensorflow/models/blob/master/research/object_detection/object_detection_tutorial.ipynb
-
-## and some is copied from Dat Tran's example at
-## https://github.com/datitran/object_detector_app/blob/master/object_detection_app.py
-
-## but I changed it to make it more understandable to me.
-
 # Import packages
 import os
-import cv2
 import numpy as np
 import tensorflow as tf
 import sys
+import cv2
 
-# This is needed since the notebook is stored in the object_detection folder.
+
 sys.path.append("..")
 
-# Import utilites
 from utils import label_map_util
 from utils import visualization_utils as vis_util
 
-# Name of the directory containing the object detection module we're using
+# Name of the directory containing the object detection module
 MODEL_NAME = 'inference_graph'
 VIDEO_NAME = 'final1.mp4'# add your video file
 
-# Grab path to current working directory
+# Current working directory
 CWD_PATH = os.getcwd()
 
 # Path to frozen detection graph .pb file, which contains the model that is used
-# for object detection.
 PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
 
-# Path to label map file
+# Path to label map
 PATH_TO_LABELS = os.path.join(CWD_PATH,'training','labelmap.pbtxt')
 
 # Path to video
 PATH_TO_VIDEO = os.path.join(CWD_PATH,VIDEO_NAME)
 
-# Number of classes the object detector can identify
+# Number of classes the object detector
 NUM_CLASSES = 1
 
 # Load the label map.
-# Label maps map indices to category names, so that when our convolution
-# network predicts `5`, we know that this corresponds to `king`.
-# Here we use internal utility functions, but anything that returns a
-# dictionary mapping integers to appropriate string labels would be fine
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
@@ -70,13 +47,10 @@ with detection_graph.as_default():
 
     sess = tf.Session(graph=detection_graph)
 
-# Define input and output tensors (i.e. data) for the object detection classifier
-
 # Input tensor is the image
 image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
 
 # Output tensors are the detection boxes, scores, and classes
-# Each box represents a part of the image where a particular object was detected
 detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
 
 # Each score represents level of confidence for each of the objects.
@@ -89,7 +63,6 @@ num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
 # Open video file
 video = cv2.VideoCapture(PATH_TO_VIDEO)
-#video.set(cv2.CAP_PROP_FPS,60)
 fps= video.get(cv2.CAP_PROP_FPS)
 print(fps)
 codec = cv2.VideoWriter_fourcc('X','V','I','D')
@@ -98,15 +71,10 @@ resolution = (640,480)
 VideoFileOutput= cv2.VideoWriter('this.avi',codec,framerate,resolution)
 
 while(video.isOpened()):
-
-    # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
-    # i.e. a single-column array, where each item in the column has the pixel RGB value
     ret, frame = video.read()
     frame_expanded = np.expand_dims(frame, axis=0)
     VideoFileOutput.write(frame)
     
-
-    # Perform the actual detection by running the model with the image as input
     (boxes, scores, classes, num) = sess.run(
         [detection_boxes, detection_scores, detection_classes, num_detections],
         feed_dict={image_tensor: frame_expanded})
@@ -122,17 +90,11 @@ while(video.isOpened()):
         line_thickness=8,
         min_score_thresh=0.98)
     
-    # All the results have been drawn on the frame, so it's time to display it.
     cv2.imshow('Object detector', frame)
     
-    
-
     # Press 'q' to quit
     if cv2.waitKey(1) == ord('q'):
         break
-
-# Clean up
-
 video.release()
 VideoFileOutput.release() 
 cv2.destroyAllWindows()
